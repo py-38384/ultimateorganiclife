cf69f1cd075bed3b8e6f2bcaf9e2128ebb8e5945
Piyal Hossein <piyal13133@gmail.com>
2024-09-10 19:00:13 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const slickPrev = document.querySelector('.slick-prev')
+const slickNext = document.querySelector('.slick-next')
+
+slickPrev.innerHTML = '<i class="fa-solid fa-arrow-left"></i>'
+slickNext.innerHTML = '<i class="fa-solid fa-arrow-right"></i>'
